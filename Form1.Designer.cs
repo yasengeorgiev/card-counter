bdb251bccdbccd209ad57cf81ad402d3796ca189
yasen.georgiev <yasen.georgiev96@gmail.com>
2021-01-08 12:18:17 +0100

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace CardCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(674, 583);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(96, 38);
+            this.btn_reset.TabIndex = 0;
+            this.btn_reset.Text = "RESET";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // btn_2
+            // 
+            this.btn_2.Location = new System.Drawing.Point(88, 36);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(75, 23);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.Text = "2";
+            this.btn_2.UseVisualStyleBackColor = true;
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.Location = new System.Drawing.Point(47, 41);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(19, 13);
+            this.lbl_2.TabIndex = 2;
+            this.lbl_2.Text = "32";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 642);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.btn_2);
+            this.Controls.Add(this.btn_reset);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.Button btn_2;
+        private System.Windows.Forms.Label lbl_2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardCounter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        string initialCount = "32";
+
+        private void btn_2_Click(object sender, EventArgs e)
+        {
+            lbl_2.Text = (Convert.ToInt32(lbl_2.Text) - 1).ToString();
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            lbl_2.Text = initialCount;
+        }
+    }
+}
