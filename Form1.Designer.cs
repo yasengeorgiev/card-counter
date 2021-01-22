8dfc30784a6a106e3140e5f5073c75d635f31bb3
yasen.georgiev <yasen.georgiev96@gmail.com>
2021-01-22 11:22:37 +0100

    main functionality of the card counter

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,24 @@ namespace CardCounter
             this.btn_reset = new System.Windows.Forms.Button();
             this.btn_2 = new System.Windows.Forms.Button();
             this.lbl_2 = new System.Windows.Forms.Label();
+            this.lbl_3 = new System.Windows.Forms.Label();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.lbl_4 = new System.Windows.Forms.Label();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.lbl_5 = new System.Windows.Forms.Label();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.lbl_6 = new System.Windows.Forms.Label();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.lbl_7 = new System.Windows.Forms.Label();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.lbl_8 = new System.Windows.Forms.Label();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.lbl_9 = new System.Windows.Forms.Label();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.lbl_10 = new System.Windows.Forms.Label();
+            this.btn_10 = new System.Windows.Forms.Button();
+            this.lbl_ace = new System.Windows.Forms.Label();
+            this.btn_ace = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_reset
@@ -63,11 +81,200 @@ namespace CardCounter
             this.lbl_2.TabIndex = 2;
             this.lbl_2.Text = "32";
             // 
+            // lbl_3
+            // 
+            this.lbl_3.AutoSize = true;
+            this.lbl_3.Location = new System.Drawing.Point(47, 90);
+            this.lbl_3.Name = "lbl_3";
+            this.lbl_3.Size = new System.Drawing.Size(19, 13);
+            this.lbl_3.TabIndex = 4;
+            this.lbl_3.Text = "32";
+            // 
+            // btn_3
+            // 
+            this.btn_3.Location = new System.Drawing.Point(88, 85);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(75, 23);
+            this.btn_3.TabIndex = 3;
+            this.btn_3.Text = "3";
+            this.btn_3.UseVisualStyleBackColor = true;
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
+            // 
+            // lbl_4
+            // 
+            this.lbl_4.AutoSize = true;
+            this.lbl_4.Location = new System.Drawing.Point(47, 138);
+            this.lbl_4.Name = "lbl_4";
+            this.lbl_4.Size = new System.Drawing.Size(19, 13);
+            this.lbl_4.TabIndex = 6;
+            this.lbl_4.Text = "32";
+            // 
+            // btn_4
+            // 
+            this.btn_4.Location = new System.Drawing.Point(88, 133);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(75, 23);
+            this.btn_4.TabIndex = 5;
+            this.btn_4.Text = "4";
+            this.btn_4.UseVisualStyleBackColor = true;
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
+            // 
+            // lbl_5
+            // 
+            this.lbl_5.AutoSize = true;
+            this.lbl_5.Location = new System.Drawing.Point(47, 187);
+            this.lbl_5.Name = "lbl_5";
+            this.lbl_5.Size = new System.Drawing.Size(19, 13);
+            this.lbl_5.TabIndex = 8;
+            this.lbl_5.Text = "32";
+            // 
+            // btn_5
+            // 
+            this.btn_5.Location = new System.Drawing.Point(88, 182);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(75, 23);
+            this.btn_5.TabIndex = 7;
+            this.btn_5.Text = "5";
+            this.btn_5.UseVisualStyleBackColor = true;
+            this.btn_5.Click += new System.EventHandler(this.btn_5_Click);
+            // 
+            // lbl_6
+            // 
+            this.lbl_6.AutoSize = true;
+            this.lbl_6.Location = new System.Drawing.Point(47, 237);
+            this.lbl_6.Name = "lbl_6";
+            this.lbl_6.Size = new System.Drawing.Size(19, 13);
+            this.lbl_6.TabIndex = 10;
+            this.lbl_6.Text = "32";
+            // 
+            // btn_6
+            // 
+            this.btn_6.Location = new System.Drawing.Point(88, 232);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(75, 23);
+            this.btn_6.TabIndex = 9;
+            this.btn_6.Text = "6";
+            this.btn_6.UseVisualStyleBackColor = true;
+            this.btn_6.Click += new System.EventHandler(this.btn_6_Click);
+            // 
+            // lbl_7
+            // 
+            this.lbl_7.AutoSize = true;
+            this.lbl_7.Location = new System.Drawing.Point(47, 293);
+            this.lbl_7.Name = "lbl_7";
+            this.lbl_7.Size = new System.Drawing.Size(19, 13);
+            this.lbl_7.TabIndex = 12;
+            this.lbl_7.Text = "32";
+            // 
+            // btn_7
+            // 
+            this.btn_7.Location = new System.Drawing.Point(88, 288);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(75, 23);
+            this.btn_7.TabIndex = 11;
+            this.btn_7.Text = "7";
+            this.btn_7.UseVisualStyleBackColor = true;
+            this.btn_7.Click += new System.EventHandler(this.btn_7_Click);
+            // 
+            // lbl_8
+            // 
+            this.lbl_8.AutoSize = true;
+            this.lbl_8.Location = new System.Drawing.Point(47, 344);
+            this.lbl_8.Name = "lbl_8";
+            this.lbl_8.Size = new System.Drawing.Size(19, 13);
+            this.lbl_8.TabIndex = 14;
+            this.lbl_8.Text = "32";
+            // 
+            // btn_8
+            // 
+            this.btn_8.Location = new System.Drawing.Point(88, 339);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(75, 23);
+            this.btn_8.TabIndex = 13;
+            this.btn_8.Text = "8";
+            this.btn_8.UseVisualStyleBackColor = true;
+            this.btn_8.Click += new System.EventHandler(this.btn_8_Click);
+            // 
+            // lbl_9
+            // 
+            this.lbl_9.AutoSize = true;
+            this.lbl_9.Location = new System.Drawing.Point(47, 393);
+            this.lbl_9.Name = "lbl_9";
+            this.lbl_9.Size = new System.Drawing.Size(19, 13);
+            this.lbl_9.TabIndex = 16;
+            this.lbl_9.Text = "32";
+            // 
+            // btn_9
+            // 
+            this.btn_9.Location = new System.Drawing.Point(88, 388);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(75, 23);
+            this.btn_9.TabIndex = 15;
+            this.btn_9.Text = "9";
+            this.btn_9.UseVisualStyleBackColor = true;
+            this.btn_9.Click += new System.EventHandler(this.btn_9_Click);
+            // 
+            // lbl_10
+            // 
+            this.lbl_10.AutoSize = true;
+            this.lbl_10.Location = new System.Drawing.Point(47, 441);
+            this.lbl_10.Name = "lbl_10";
+            this.lbl_10.Size = new System.Drawing.Size(25, 13);
+            this.lbl_10.TabIndex = 18;
+            this.lbl_10.Text = "128";
+            // 
+            // btn_10
+            // 
+            this.btn_10.Location = new System.Drawing.Point(88, 436);
+            this.btn_10.Name = "btn_10";
+            this.btn_10.Size = new System.Drawing.Size(75, 23);
+            this.btn_10.TabIndex = 17;
+            this.btn_10.Text = "10";
+            this.btn_10.UseVisualStyleBackColor = true;
+            this.btn_10.Click += new System.EventHandler(this.btn_10_Click);
+            // 
+            // lbl_ace
+            // 
+            this.lbl_ace.AutoSize = true;
+            this.lbl_ace.Location = new System.Drawing.Point(47, 487);
+            this.lbl_ace.Name = "lbl_ace";
+            this.lbl_ace.Size = new System.Drawing.Size(19, 13);
+            this.lbl_ace.TabIndex = 20;
+            this.lbl_ace.Text = "32";
+            // 
+            // btn_ace
+            // 
+            this.btn_ace.Location = new System.Drawing.Point(88, 482);
+            this.btn_ace.Name = "btn_ace";
+            this.btn_ace.Size = new System.Drawing.Size(75, 23);
+            this.btn_ace.TabIndex = 19;
+            this.btn_ace.Text = "ACE";
+            this.btn_ace.UseVisualStyleBackColor = true;
+            this.btn_ace.Click += new System.EventHandler(this.btn_ace_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(791, 642);
+            this.Controls.Add(this.lbl_ace);
+            this.Controls.Add(this.btn_ace);
+            this.Controls.Add(this.lbl_10);
+            this.Controls.Add(this.btn_10);
+            this.Controls.Add(this.lbl_9);
+            this.Controls.Add(this.btn_9);
+            this.Controls.Add(this.lbl_8);
+            this.Controls.Add(this.btn_8);
+            this.Controls.Add(this.lbl_7);
+            this.Controls.Add(this.btn_7);
+            this.Controls.Add(this.lbl_6);
+            this.Controls.Add(this.btn_6);
+            this.Controls.Add(this.lbl_5);
+            this.Controls.Add(this.btn_5);
+            this.Controls.Add(this.lbl_4);
+            this.Controls.Add(this.btn_4);
+            this.Controls.Add(this.lbl_3);
+            this.Controls.Add(this.btn_3);
             this.Controls.Add(this.lbl_2);
             this.Controls.Add(this.btn_2);
             this.Controls.Add(this.btn_reset);
@@ -83,6 +290,24 @@ namespace CardCounter
         private System.Windows.Forms.Button btn_reset;
         private System.Windows.Forms.Button btn_2;
         private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.Label lbl_3;
+        private System.Windows.Forms.Button btn_3;
+        private System.Windows.Forms.Label lbl_4;
+        private System.Windows.Forms.Button btn_4;
+        private System.Windows.Forms.Label lbl_5;
+        private System.Windows.Forms.Button btn_5;
+        private System.Windows.Forms.Label lbl_6;
+        private System.Windows.Forms.Button btn_6;
+        private System.Windows.Forms.Label lbl_7;
+        private System.Windows.Forms.Button btn_7;
+        private System.Windows.Forms.Label lbl_8;
+        private System.Windows.Forms.Button btn_8;
+        private System.Windows.Forms.Label lbl_9;
+        private System.Windows.Forms.Button btn_9;
+        private System.Windows.Forms.Label lbl_10;
+        private System.Windows.Forms.Button btn_10;
+        private System.Windows.Forms.Label lbl_ace;
+        private System.Windows.Forms.Button btn_ace;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,19 +15,89 @@ namespace CardCounter
         public Form1()
         {
             InitializeComponent();
+            //8 decks
+            //all cards: 416 (4*13*8)
 
         }
 
         string initialCount = "32";
+        string initialCountForTens = "128";
 
         private void btn_2_Click(object sender, EventArgs e)
         {
             lbl_2.Text = (Convert.ToInt32(lbl_2.Text) - 1).ToString();
         }
 
+        private void btn_3_Click(object sender, EventArgs e)
+        {
+            lbl_3.Text = (Convert.ToInt32(lbl_3.Text) - 1).ToString();
+        }
+
+        private void btn_4_Click(object sender, EventArgs e)
+        {
+            lbl_4.Text = (Convert.ToInt32(lbl_4.Text) - 1).ToString();
+        }
+
+        private void btn_5_Click(object sender, EventArgs e)
+        {
+            lbl_5.Text = (Convert.ToInt32(lbl_5.Text) - 1).ToString();
+        }
+
+        private void btn_6_Click(object sender, EventArgs e)
+        {
+            lbl_6.Text = (Convert.ToInt32(lbl_6.Text) - 1).ToString();
+        }
+
+        private void btn_7_Click(object sender, EventArgs e)
+        {
+            lbl_7.Text = (Convert.ToInt32(lbl_7.Text) - 1).ToString();
+        }
+
+        private void btn_8_Click(object sender, EventArgs e)
+        {
+            lbl_8.Text = (Convert.ToInt32(lbl_8.Text) - 1).ToString();
+        }
+
+        private void btn_9_Click(object sender, EventArgs e)
+        {
+            lbl_9.Text = (Convert.ToInt32(lbl_9.Text) - 1).ToString();
+        }
+
+        private void btn_10_Click(object sender, EventArgs e)
+        {
+            lbl_10.Text = (Convert.ToInt32(lbl_10.Text) - 1).ToString();
+        }
+
+        private void btn_ace_Click(object sender, EventArgs e)
+        {
+            lbl_ace.Text = (Convert.ToInt32(lbl_ace.Text) - 1).ToString();
+        }
+
+        //method for resetting the label values
+        private void resetLabels(Label lbl)
+        {
+            //setting the count of cards to 128 (all 10s, jacks, queens, kings)
+            if (lbl.Name == "lbl_10")
+            {
+                lbl.Text = initialCountForTens;
+            }
+            //setting the count of cards to 32
+            else
+            {
+                lbl.Text = initialCount;
+            }
+        }
+
         private void btn_reset_Click(object sender, EventArgs e)
         {
-            lbl_2.Text = initialCount;
+            foreach (Control lbl in this.Controls)
+            {
+                if (lbl is Label)
+                {
+                    resetLabels((Label)lbl);
+                }
+            }
+
         }
     }
 }
