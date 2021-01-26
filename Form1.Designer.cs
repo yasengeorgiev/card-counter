9108bc583a649013a9ab96e7f3a04aa0bdd9018c
yasen.georgiev <yasen.georgiev96@gmail.com>
2021-01-26 17:54:30 +0100

    work on chances functionality

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,6 +50,18 @@ namespace CardCounter
             this.btn_10 = new System.Windows.Forms.Button();
             this.lbl_ace = new System.Windows.Forms.Label();
             this.btn_ace = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_chances_2 = new System.Windows.Forms.Label();
+            this.lbl_chances_3 = new System.Windows.Forms.Label();
+            this.lbl_chances_4 = new System.Windows.Forms.Label();
+            this.lbl_chances_5 = new System.Windows.Forms.Label();
+            this.lbl_chances_6 = new System.Windows.Forms.Label();
+            this.lbl_chances_7 = new System.Windows.Forms.Label();
+            this.lbl_chances_8 = new System.Windows.Forms.Label();
+            this.lbl_chances_9 = new System.Windows.Forms.Label();
+            this.lbl_chances_10 = new System.Windows.Forms.Label();
+            this.lbl_chances_ace = new System.Windows.Forms.Label();
+            this.lbl_remaining_cards = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_reset
@@ -64,7 +76,7 @@ namespace CardCounter
             // 
             // btn_2
             // 
-            this.btn_2.Location = new System.Drawing.Point(88, 36);
+            this.btn_2.Location = new System.Drawing.Point(84, 86);
             this.btn_2.Name = "btn_2";
             this.btn_2.Size = new System.Drawing.Size(75, 23);
             this.btn_2.TabIndex = 1;
@@ -75,7 +87,7 @@ namespace CardCounter
             // lbl_2
             // 
             this.lbl_2.AutoSize = true;
-            this.lbl_2.Location = new System.Drawing.Point(47, 41);
+            this.lbl_2.Location = new System.Drawing.Point(43, 91);
             this.lbl_2.Name = "lbl_2";
             this.lbl_2.Size = new System.Drawing.Size(19, 13);
             this.lbl_2.TabIndex = 2;
@@ -84,7 +96,7 @@ namespace CardCounter
             // lbl_3
             // 
             this.lbl_3.AutoSize = true;
-            this.lbl_3.Location = new System.Drawing.Point(47, 90);
+            this.lbl_3.Location = new System.Drawing.Point(43, 140);
             this.lbl_3.Name = "lbl_3";
             this.lbl_3.Size = new System.Drawing.Size(19, 13);
             this.lbl_3.TabIndex = 4;
@@ -92,7 +104,7 @@ namespace CardCounter
             // 
             // btn_3
             // 
-            this.btn_3.Location = new System.Drawing.Point(88, 85);
+            this.btn_3.Location = new System.Drawing.Point(84, 135);
             this.btn_3.Name = "btn_3";
             this.btn_3.Size = new System.Drawing.Size(75, 23);
             this.btn_3.TabIndex = 3;
@@ -103,7 +115,7 @@ namespace CardCounter
             // lbl_4
             // 
             this.lbl_4.AutoSize = true;
-            this.lbl_4.Location = new System.Drawing.Point(47, 138);
+            this.lbl_4.Location = new System.Drawing.Point(43, 188);
             this.lbl_4.Name = "lbl_4";
             this.lbl_4.Size = new System.Drawing.Size(19, 13);
             this.lbl_4.TabIndex = 6;
@@ -111,7 +123,7 @@ namespace CardCounter
             // 
             // btn_4
             // 
-            this.btn_4.Location = new System.Drawing.Point(88, 133);
+            this.btn_4.Location = new System.Drawing.Point(84, 183);
             this.btn_4.Name = "btn_4";
             this.btn_4.Size = new System.Drawing.Size(75, 23);
             this.btn_4.TabIndex = 5;
@@ -122,7 +134,7 @@ namespace CardCounter
             // lbl_5
             // 
             this.lbl_5.AutoSize = true;
-            this.lbl_5.Location = new System.Drawing.Point(47, 187);
+            this.lbl_5.Location = new System.Drawing.Point(43, 237);
             this.lbl_5.Name = "lbl_5";
             this.lbl_5.Size = new System.Drawing.Size(19, 13);
             this.lbl_5.TabIndex = 8;
@@ -130,7 +142,7 @@ namespace CardCounter
             // 
             // btn_5
             // 
-            this.btn_5.Location = new System.Drawing.Point(88, 182);
+            this.btn_5.Location = new System.Drawing.Point(84, 232);
             this.btn_5.Name = "btn_5";
             this.btn_5.Size = new System.Drawing.Size(75, 23);
             this.btn_5.TabIndex = 7;
@@ -141,7 +153,7 @@ namespace CardCounter
             // lbl_6
             // 
             this.lbl_6.AutoSize = true;
-            this.lbl_6.Location = new System.Drawing.Point(47, 237);
+            this.lbl_6.Location = new System.Drawing.Point(43, 287);
             this.lbl_6.Name = "lbl_6";
             this.lbl_6.Size = new System.Drawing.Size(19, 13);
             this.lbl_6.TabIndex = 10;
@@ -149,7 +161,7 @@ namespace CardCounter
             // 
             // btn_6
             // 
-            this.btn_6.Location = new System.Drawing.Point(88, 232);
+            this.btn_6.Location = new System.Drawing.Point(84, 282);
             this.btn_6.Name = "btn_6";
             this.btn_6.Size = new System.Drawing.Size(75, 23);
             this.btn_6.TabIndex = 9;
@@ -160,7 +172,7 @@ namespace CardCounter
             // lbl_7
             // 
             this.lbl_7.AutoSize = true;
-            this.lbl_7.Location = new System.Drawing.Point(47, 293);
+            this.lbl_7.Location = new System.Drawing.Point(43, 343);
             this.lbl_7.Name = "lbl_7";
             this.lbl_7.Size = new System.Drawing.Size(19, 13);
             this.lbl_7.TabIndex = 12;
@@ -168,7 +180,7 @@ namespace CardCounter
             // 
             // btn_7
             // 
-            this.btn_7.Location = new System.Drawing.Point(88, 288);
+            this.btn_7.Location = new System.Drawing.Point(84, 338);
             this.btn_7.Name = "btn_7";
             this.btn_7.Size = new System.Drawing.Size(75, 23);
             this.btn_7.TabIndex = 11;
@@ -179,7 +191,7 @@ namespace CardCounter
             // lbl_8
             // 
             this.lbl_8.AutoSize = true;
-            this.lbl_8.Location = new System.Drawing.Point(47, 344);
+            this.lbl_8.Location = new System.Drawing.Point(43, 394);
             this.lbl_8.Name = "lbl_8";
             this.lbl_8.Size = new System.Drawing.Size(19, 13);
             this.lbl_8.TabIndex = 14;
@@ -187,7 +199,7 @@ namespace CardCounter
             // 
             // btn_8
             // 
-            this.btn_8.Location = new System.Drawing.Point(88, 339);
+            this.btn_8.Location = new System.Drawing.Point(84, 389);
             this.btn_8.Name = "btn_8";
             this.btn_8.Size = new System.Drawing.Size(75, 23);
             this.btn_8.TabIndex = 13;
@@ -198,7 +210,7 @@ namespace CardCounter
             // lbl_9
             // 
             this.lbl_9.AutoSize = true;
-            this.lbl_9.Location = new System.Drawing.Point(47, 393);
+            this.lbl_9.Location = new System.Drawing.Point(43, 443);
             this.lbl_9.Name = "lbl_9";
             this.lbl_9.Size = new System.Drawing.Size(19, 13);
             this.lbl_9.TabIndex = 16;
@@ -206,7 +218,7 @@ namespace CardCounter
             // 
             // btn_9
             // 
-            this.btn_9.Location = new System.Drawing.Point(88, 388);
+            this.btn_9.Location = new System.Drawing.Point(84, 438);
             this.btn_9.Name = "btn_9";
             this.btn_9.Size = new System.Drawing.Size(75, 23);
             this.btn_9.TabIndex = 15;
@@ -217,7 +229,7 @@ namespace CardCounter
             // lbl_10
             // 
             this.lbl_10.AutoSize = true;
-            this.lbl_10.Location = new System.Drawing.Point(47, 441);
+            this.lbl_10.Location = new System.Drawing.Point(43, 491);
             this.lbl_10.Name = "lbl_10";
             this.lbl_10.Size = new System.Drawing.Size(25, 13);
             this.lbl_10.TabIndex = 18;
@@ -225,7 +237,7 @@ namespace CardCounter
             // 
             // btn_10
             // 
-            this.btn_10.Location = new System.Drawing.Point(88, 436);
+            this.btn_10.Location = new System.Drawing.Point(84, 486);
             this.btn_10.Name = "btn_10";
             this.btn_10.Size = new System.Drawing.Size(75, 23);
             this.btn_10.TabIndex = 17;
@@ -236,7 +248,7 @@ namespace CardCounter
             // lbl_ace
             // 
             this.lbl_ace.AutoSize = true;
-            this.lbl_ace.Location = new System.Drawing.Point(47, 487);
+            this.lbl_ace.Location = new System.Drawing.Point(43, 537);
             this.lbl_ace.Name = "lbl_ace";
             this.lbl_ace.Size = new System.Drawing.Size(19, 13);
             this.lbl_ace.TabIndex = 20;
@@ -244,7 +256,7 @@ namespace CardCounter
             // 
             // btn_ace
             // 
-            this.btn_ace.Location = new System.Drawing.Point(88, 482);
+            this.btn_ace.Location = new System.Drawing.Point(84, 532);
             this.btn_ace.Name = "btn_ace";
             this.btn_ace.Size = new System.Drawing.Size(75, 23);
             this.btn_ace.TabIndex = 19;
@@ -252,11 +264,131 @@ namespace CardCounter
             this.btn_ace.UseVisualStyleBackColor = true;
             this.btn_ace.Click += new System.EventHandler(this.btn_ace_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Chances";
+            // 
+            // lbl_chances_2
+            // 
+            this.lbl_chances_2.AutoSize = true;
+            this.lbl_chances_2.Location = new System.Drawing.Point(209, 86);
+            this.lbl_chances_2.Name = "lbl_chances_2";
+            this.lbl_chances_2.Size = new System.Drawing.Size(35, 13);
+            this.lbl_chances_2.TabIndex = 22;
+            this.lbl_chances_2.Text = "label2";
+            // 
+            // lbl_chances_3
+            // 
+            this.lbl_chances_3.AutoSize = true;
+            this.lbl_chances_3.Location = new System.Drawing.Point(209, 135);
+            this.lbl_chances_3.Name = "lbl_chances_3";
+            this.lbl_chances_3.Size = new System.Drawing.Size(35, 13);
+            this.lbl_chances_3.TabIndex = 23;
+            this.lbl_chances_3.Text = "label3";
+            // 
+            // lbl_chances_4
+            // 
+            this.lbl_chances_4.AutoSize = true;
+            this.lbl_chances_4.Location = new System.Drawing.Point(209, 183);
+            this.lbl_chances_4.Name = "lbl_chances_4";
+            this.lbl_chances_4.Size = new System.Drawing.Size(35, 13);
+            this.lbl_chances_4.TabIndex = 24;
+            this.lbl_chances_4.Text = "label4";
+            // 
+            // lbl_chances_5
+            // 
+            this.lbl_chances_5.AutoSize = true;
+            this.lbl_chances_5.Location = new System.Drawing.Point(209, 232);
+            this.lbl_chances_5.Name = "lbl_chances_5";
+            this.lbl_chances_5.Size = new System.Drawing.Size(35, 13);
+            this.lbl_chances_5.TabIndex = 25;
+            this.lbl_chances_5.Text = "label5";
+            // 
+            // lbl_chances_6
+            // 
+            this.lbl_chances_6.AutoSize = true;
+            this.lbl_chances_6.Location = new System.Drawing.Point(209, 287);
+            this.lbl_chances_6.Name = "lbl_chances_6";
+            this.lbl_chances_6.Size = new System.Drawing.Size(35, 13);
+            this.lbl_chances_6.TabIndex = 26;
+            this.lbl_chances_6.Text = "label6";
+            // 
+            // lbl_chances_7
+            // 
+            this.lbl_chances_7.AutoSize = true;
+            this.lbl_chances_7.Location = new System.Drawing.Point(209, 343);
+            this.lbl_chances_7.Name = "lbl_chances_7";
+            this.lbl_chances_7.Size = new System.Drawing.Size(35, 13);
+            this.lbl_chances_7.TabIndex = 27;
+            this.lbl_chances_7.Text = "label7";
+            // 
+            // lbl_chances_8
+            // 
+            this.lbl_chances_8.AutoSize = true;
+            this.lbl_chances_8.Location = new System.Drawing.Point(209, 394);
+            this.lbl_chances_8.Name = "lbl_chances_8";
+            this.lbl_chances_8.Size = new System.Drawing.Size(35, 13);
+            this.lbl_chances_8.TabIndex = 28;
+            this.lbl_chances_8.Text = "label8";
+            // 
+            // lbl_chances_9
+            // 
+            this.lbl_chances_9.AutoSize = true;
+            this.lbl_chances_9.Location = new System.Drawing.Point(209, 443);
+            this.lbl_chances_9.Name = "lbl_chances_9";
+            this.lbl_chances_9.Size = new System.Drawing.Size(35, 13);
+            this.lbl_chances_9.TabIndex = 29;
+            this.lbl_chances_9.Text = "label9";
+            // 
+            // lbl_chances_10
+            // 
+            this.lbl_chances_10.AutoSize = true;
+            this.lbl_chances_10.Location = new System.Drawing.Point(209, 491);
+            this.lbl_chances_10.Name = "lbl_chances_10";
+            this.lbl_chances_10.Size = new System.Drawing.Size(41, 13);
+            this.lbl_chances_10.TabIndex = 30;
+            this.lbl_chances_10.Text = "label10";
+            // 
+            // lbl_chances_ace
+            // 
+            this.lbl_chances_ace.AutoSize = true;
+            this.lbl_chances_ace.Location = new System.Drawing.Point(209, 537);
+            this.lbl_chances_ace.Name = "lbl_chances_ace";
+            this.lbl_chances_ace.Size = new System.Drawing.Size(41, 13);
+            this.lbl_chances_ace.TabIndex = 31;
+            this.lbl_chances_ace.Text = "label11";
+            // 
+            // lbl_remaining_cards
+            // 
+            this.lbl_remaining_cards.AutoSize = true;
+            this.lbl_remaining_cards.Location = new System.Drawing.Point(708, 86);
+            this.lbl_remaining_cards.Name = "lbl_remaining_cards";
+            this.lbl_remaining_cards.Size = new System.Drawing.Size(25, 13);
+            this.lbl_remaining_cards.TabIndex = 32;
+            this.lbl_remaining_cards.Text = "416";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(791, 642);
+            this.Controls.Add(this.lbl_remaining_cards);
+            this.Controls.Add(this.lbl_chances_ace);
+            this.Controls.Add(this.lbl_chances_10);
+            this.Controls.Add(this.lbl_chances_9);
+            this.Controls.Add(this.lbl_chances_8);
+            this.Controls.Add(this.lbl_chances_7);
+            this.Controls.Add(this.lbl_chances_6);
+            this.Controls.Add(this.lbl_chances_5);
+            this.Controls.Add(this.lbl_chances_4);
+            this.Controls.Add(this.lbl_chances_3);
+            this.Controls.Add(this.lbl_chances_2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lbl_ace);
             this.Controls.Add(this.btn_ace);
             this.Controls.Add(this.lbl_10);
@@ -308,6 +440,18 @@ namespace CardCounter
         private System.Windows.Forms.Button btn_10;
         private System.Windows.Forms.Label lbl_ace;
         private System.Windows.Forms.Button btn_ace;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_chances_2;
+        private System.Windows.Forms.Label lbl_chances_3;
+        private System.Windows.Forms.Label lbl_chances_4;
+        private System.Windows.Forms.Label lbl_chances_5;
+        private System.Windows.Forms.Label lbl_chances_6;
+        private System.Windows.Forms.Label lbl_chances_7;
+        private System.Windows.Forms.Label lbl_chances_8;
+        private System.Windows.Forms.Label lbl_chances_9;
+        private System.Windows.Forms.Label lbl_chances_10;
+        private System.Windows.Forms.Label lbl_chances_ace;
+        private System.Windows.Forms.Label lbl_remaining_cards;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -17,20 +17,39 @@ namespace CardCounter
             InitializeComponent();
             //8 decks
             //all cards: 416 (4*13*8)
-
+            //lbl_remaining_cards.Text = "416";
         }
+
+        //public int card2 = 32;
 
         string initialCount = "32";
         string initialCountForTens = "128";
+        double allCards = 416;
+        double card_2 = 32;
+        
+        //this method calculates the remaining number of cards of specific kind (for example all the aces)
+        private double dealingCards(double card)
+        {
+            card -= 1;
+            return card;
+        } 
 
         private void btn_2_Click(object sender, EventArgs e)
         {
-            lbl_2.Text = (Convert.ToInt32(lbl_2.Text) - 1).ToString();
+            //lbl_2.Text = (Convert.ToInt32(lbl_2.Text) - 1).ToString();
+
+            lbl_2.Text = dealingCards(card_2).ToString();
+            card_2--;
+            allCards--;
+
+            //lbl_2.text = (card2 - 1).tostring();
+            //card2--;
+            calculateChances();
         }
 
         private void btn_3_Click(object sender, EventArgs e)
         {
-            lbl_3.Text = (Convert.ToInt32(lbl_3.Text) - 1).ToString();
+            //lbl_3.Text = (Convert.ToInt32(lbl_3.Text) - 1).ToString();
         }
 
         private void btn_4_Click(object sender, EventArgs e)
@@ -98,6 +117,20 @@ namespace CardCounter
                 }
             }
 
+        }
+
+        // this method is going to calculate what are the chances for a certain card to be dealt
+        private void calculateChances()
+        {
+            Console.WriteLine(card_2);
+            Console.WriteLine(allCards);
+            Console.WriteLine(card_2/allCards);
+            lbl_chances_2.Text = Math.Round(((card_2 / allCards) * 100), 2).ToString();
+
+            Console.WriteLine("------------------");
+            Console.WriteLine(card_2);
+            Console.WriteLine(allCards);
+            Console.WriteLine(lbl_chances_2.Text);
         }
     }
 }
